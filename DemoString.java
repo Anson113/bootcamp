f3e80f2fa667fc53487141c124ff74532c5c887b
Anson Chow <siuon1113@gmail.com>
2024-05-08 21:49:25 +0800

    update

---
+++ DemoString.java
@@ -0,0 +1,14 @@
+public class DemoString {
+
+    public static void main (String[] args) {
+        String x = "hello";
+        String y = "^^^";
+        String dollar = "$10";
+
+        String withSpace ="hellow";
+        System.out.println(withSpace);
+        System.out.println(y);
+
+    }
+    
+}
